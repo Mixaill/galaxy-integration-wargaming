284efb6205143075031406740d481b406f6533f7
Mikhail Paulyshka <me@mixaill.tk>
2019-10-29 01:51:19 +0300

    git: update repo according to FoG recomendation

--- 3rdparty/slixmpp/plugins/xep_0009/stanza/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-    Slixmpp: The Slick XMPP Library
-    Copyright (C) 2011 Nathanael C. Fritz, Dann Martens (TOMOTON).
-    This file is part of Slixmpp.
-
-    See the file LICENSE for copying permission.
-"""
-
-from slixmpp.plugins.xep_0009.stanza.RPC import RPCQuery, MethodCall, MethodResponse
--- 3rdparty/slixmpp/thirdparty/__init__.py
+++
@@ -1,7 +0,0 @@
-try:
-    from gnupg import GPG
-except:
-    from slixmpp.thirdparty.gnupg import GPG
-
-from slixmpp.thirdparty.mini_dateutil import tzutc, tzoffset, parse_iso
-from slixmpp.thirdparty.orderedset import OrderedSet
