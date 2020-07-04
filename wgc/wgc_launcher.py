27ea3caaab5015a440d5d9cafd32bde5923c204f
Mikhail Paulyshka <me@mixaill.tk>
2020-07-04 23:51:55 +0300

    wgc_launcher: add `launch_app` function

--- wgc/wgc_launcher.py
+++ wgc/wgc_launcher.py
@@ -3,6 +3,7 @@
 
 import logging
 import subprocess
+from typing import List, Optional
 
 from .wgc_helper import get_platform, DETACHED_PROCESS
 from .wgc_location import WGCLocation
@@ -55,3 +56,38 @@ class WgcLauncher:
         subprocess.Popen([WGCLocation.get_wgc_exe_path(), '--install', '-g', install_uri, '--skipJobCheck'], creationflags=DETACHED_PROCESS)
         return True
 
+    #
+    # WGC Application Run
+
+    @staticmethod
+    def launch_app(executable_path: str, executable_arguments: Optional[List[str]] = None) -> bool:
+        if get_platform() == "macos":
+            return WgcLauncher.__launch_app_macos(executable_path, executable_arguments)
+        elif get_platform() == 'windows':
+            return WgcLauncher.__launch_app_windows(executable_path, executable_arguments)
+        else:
+            logging.getLogger('wgc_launcher').warning('launch_game: unsupported platform (%s)' % (get_platform()))
+            return False
+
+    @staticmethod
+    def __launch_app_macos(executable_path: str, executable_arguments: Optional[List[str]]) -> bool:
+        args = [
+            WGCLocation.get_wgc_wine_macos_path(), '--bottle', 'default', '--wait-children', '--enable-alt-loader', '--macdrv',
+            '--workdir', WGCLocation.FALLBACK_DIR_WGC,
+            executable_path
+        ]
+
+        if executable_arguments:
+            args += executable_arguments
+  
+        subprocess.Popen(args, close_fds=True)
+        return True
+
+    @staticmethod
+    def __launch_app_windows(executable_path: str, executable_arguments: Optional[List[str]]) -> bool:
+        args = [executable_path]
+        if executable_arguments:
+            args += executable_arguments
+
+        subprocess.Popen(args, creationflags=DETACHED_PROCESS)
+        return True
