fc96af798bb95777d554737c7c60c6dd80d81dd4
Mikhail Paulyshka <me@mixaill.tk>
2020-07-04 23:51:23 +0300

    wgc_location: add `get_wgc_wine_macos_path` and `get_wgc_wgcapi_path`

--- wgc/wgc_location.py
+++ wgc/wgc_location.py
@@ -9,8 +9,10 @@ from typing import List
 from .wgc_helper import scantree, get_platform
 
 class WGCLocation():    
-    MACOS_EXECUTABLE     = '/Applications/Wargaming.net Game Center.app/Contents/MacOS/Wargaming.net Game Center'
-    WINE_DRIVE_C         = os.path.join(Path.home(), 'Library/Application Support/Wargaming.net Game Center/Bottles/wargaminggamecenter64/drive_c/')
+    MACOS_COMMON_ROOT    = '/Applications/Wargaming.net Game Center.app/'
+    MACOS_COMMON_BINARY  = os.path.join(MACOS_COMMON_ROOT, 'Contents/MacOS/Wargaming.net Game Center')
+    MACOS_COMMON_WINE    = os.path.join(MACOS_COMMON_ROOT, 'Contents/SharedSupport/wargaminggamecenter/bin/wine')
+    MACOS_USER_ROOT      = os.path.join(Path.home(), 'Library/Application Support/Wargaming.net Game Center/Bottles/wargaminggamecenter64/drive_c/')
     
     FALLBACK_DIR_PROGRAMDATA = 'C:/ProgramData/'
     FALLBACK_DIR_WGC         = 'C:/Program Files (x86)/Wargaming.net/GameCenter/'
@@ -22,7 +24,8 @@ class WGCLocation():
 
     WGC_WGCDIR_EXECUTABLE = 'wgc.exe'
     WGC_WGCDIR_GAMESRESTRICTIONS = 'games_restrictions.xml'
-    WGC_WGCDIR_PREFERENCES = 'preferences.xml'  
+    WGC_WGCDIR_PREFERENCES = 'preferences.xml'
+    WGC_WGCDIR_WGCAPI = 'wgc_api/wgc_api.exe'
 
     @staticmethod 
     def fixup_path(fspath: str) -> str:
@@ -30,7 +33,7 @@ class WGCLocation():
         converts Wine path to macOS path
         '''
         if get_platform() == "macos":
-            return fspath.replace('\\','/').replace('C:/', WGCLocation.WINE_DRIVE_C)
+            return fspath.replace('\\','/').replace('C:/', WGCLocation.MACOS_USER_ROOT)
 
         return fspath
 
@@ -40,7 +43,7 @@ class WGCLocation():
         converts macOS path to Wine path
         '''
         if get_platform() == "macos":
-            return fspath.replace('\\','/').replace(WGCLocation.WINE_DRIVE_C,'C:/')
+            return fspath.replace('\\','/').replace(WGCLocation.MACOS_USER_ROOT,'C:/')
 
         return fspath
 
@@ -125,7 +128,18 @@ class WGCLocation():
             logging.getLogger('wgc_location').error('get_wgc_exe_macos_path: requires macOS')
             return ''
     
-        return WGCLocation.MACOS_EXECUTABLE
+        return WGCLocation.MACOS_COMMON_BINARY
+
+    @staticmethod
+    def get_wgc_wine_macos_path() -> str:
+        '''
+        returns path to the WGC's wine binary
+        '''
+        if get_platform() != 'macos':
+            logging.getLogger('wgc_location').error('get_wgc_exe_macos_path: requires macOS')
+            return ''
+    
+        return WGCLocation.MACOS_COMMON_WINE
 
     @staticmethod
     def get_wgc_preferences_file() -> str:
@@ -138,6 +152,19 @@ class WGCLocation():
             logging.getLogger('wgc_location').warning('get_wgc_preferences_file: failed to find wgc preferences.xml')
             return ''
         
+        return result
+
+    @staticmethod
+    def get_wgc_wgcapi_path() -> str:
+        '''
+        returns path to the wgc_api.exe file
+        '''
+        
+        result = os.path.join(WGCLocation.get_wgc_dir(), WGCLocation.WGC_WGCDIR_WGCAPI)
+        if not os.path.exists(result):
+            logging.getLogger('wgc_location').warning('get_wgc_wgcapi_path: failed to find wgc_api.exe')
+            return ''
+
         return result
 
     @staticmethod
